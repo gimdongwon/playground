329bc7824dca16c76a85399a19f210e7e67a61d9
kimdongwon <ehddnjs8989@gmail.com>
2020-12-14 03:30:22 +0900

    linear xor double overlap v2

--- tensorflow/lab_09_NeuralNet.py
+++ tensorflow/lab_09_NeuralNet.py
@@ -12,11 +12,15 @@ y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
 X = tf.placeholder(tf.float32, [None, 2])
 Y = tf.placeholder(tf.float32, [None, 1])
 
-W = tf.Variable(tf.random_normal([2, 1]), name="weight")
-b = tf.Variable(tf.random_normal([1]), name="bias")
+W1 = tf.Variable(tf.random_normal([2, 2]), name="weight1")
+b1 = tf.Variable(tf.random_normal([1]), name="bias1")
+layer1 = tf.sigmoid(tf.matmul(X, W1)+b1)
+
+W2 = tf.Variable(tf.random_normal([2, 1]), name="weight2")
+b2 = tf.Variable(tf.random_normal([1]), name="bias2")
 
 # Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
-hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
 
 # cost/loss function
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y)
@@ -34,11 +38,12 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for step in range(10001):
-        _, cost_val, w_val = sess.run(
-            [train, cost, W], feed_dict={X: x_data, Y: y_data}
+        sess.run(
+            train, feed_dict={X: x_data, Y: y_data}
         )
         if step % 100 == 0:
-            print(step, cost_val, w_val)
+            print(step, sess.run(cost, feed_dict={
+                  X: x_data, Y: y_data}), sess.run([W1, W2]))
 
     # Accuracy report
     h, c, a = sess.run(
