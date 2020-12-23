280ef9136dc1466cfd39c3b393a6797bffb1992c
kimdongwon <ehddnjs8989@gmail.com>
2020-12-24 04:01:12 +0900

    add greedy

---
+++ algorithm/greedy.py
@@ -0,0 +1,23 @@
+# 함수 작성
+
+def bigNum():
+    N,M,K = map(int,input().split())
+    data = list(map(int, input().split()))
+
+    maxNum = max(data)
+    data.remove(maxNum)
+    secondMaxNum = max(data)
+    
+    result = 0
+    duplication = 0
+    for i in range(M):
+        duplication +=1
+        if duplication <= K:
+            result += maxNum
+        else:
+            result+=secondMaxNum
+            duplication = 0
+    print(result)
+
+# 결과 출력
+bigNum()
