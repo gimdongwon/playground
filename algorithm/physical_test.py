ae46f01e62f022fd08a39a92b23823fa37dedd2b
kimdongwon <ehddnjs8989@gmail.com>
2021-01-19 11:12:44 +0900

    치킨배달

--- algorithm/physical_test.py
+++ algorithm/physical_test.py
@@ -43,7 +43,7 @@ def resorted():
     print(result2 + str(result))
 
 
-resorted()
+# resorted()
 
 # ----
 
@@ -250,3 +250,42 @@ def solution(n, build_frame):
 
     answer.sort()
     return answer
+
+def chicken():
+    from itertools import combinations
+    N,M = map(int, input().split())
+    arr = [list(map(int, input())) for _ in range(N)]
+    
+    home = []
+    chickenHome = []
+    distance = 0
+    distanceZip = []
+
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 1:
+                home.append([i,j])
+            elif arr[i][j] == 2:
+                chickenHome.append([i,j])
+    result = chickenHome
+    if M != 1:
+        result = list(combinations(chickenHome, M))
+        for resultItem in result:
+            distance = 0
+            for homeItem in home:
+                temp = []
+                for resultItem2 in resultItem:
+                    temp.append(abs(resultItem2[0]-homeItem[0]) + abs(resultItem2[1]-homeItem[1]))
+                distance += min(temp)
+            distanceZip.append(distance)
+    else:
+        for resultItem in result:
+            distance = 0
+            for homeItem in home:
+                distance += abs(resultItem[0] - homeItem[0]) + abs(resultItem[1] - homeItem[1])
+            distanceZip.append(distance)
+    # print(result)
+    print(min(distanceZip))
+    
+
+chicken()
